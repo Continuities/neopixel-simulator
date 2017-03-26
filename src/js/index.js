f47db3928284e95a6452f8ae31bce27e81a9407b
Continuities <michael@doublespeakgames.com>
2017-03-26 12:43:24 -0400

    up and running

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import Neopixel from './neopixel';
+import css from '../css/neopixel.css';
+
+document.body.innerHTML += Neopixel.foo();
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: "bundle.js"
+  },
+  devtool: 'source-map',
+  devServer:{
+    contentBase: 'dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+};
